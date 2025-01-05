43f573fd21025b374a8c1f0b663285693ac69a8c
Aaron Bentley <aaron@aaronbentley.com>
2025-01-05 15:27:31 -0500

    Enable teamviewer

--- graphical.nix
+++ graphical.nix
@@ -9,6 +9,9 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Enable TeamViewer
+  services.teamviewer.enable = true;
+
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
