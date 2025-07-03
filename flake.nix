ad2cf326d61076db2a3497d9291896b0186605ae
Aaron Bentley <aaron@aaronbentley.com>
2025-07-03 02:04:06 +0000

    Add nixos-wsl as flake

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 
   outputs =
@@ -12,6 +13,7 @@
       self,
       nixpkgs,
       home-manager,
+      nixos-wsl,
     }:
     {
       nixosConfigurations.thinky = import ./git/thinky/flk.nix {
@@ -23,6 +25,12 @@
         self = self;
         nixpkgs = nixpkgs;
         home-manager = home-manager;
+      };
+      nixosConfigurations.gamey-wsl = import ./git/gamey-wsl/flk.nix {
+        self = self;
+        nixpkgs = nixpkgs;
+        home-manager = home-manager;
+        nixos-wsl = nixos-wsl;
       };
     };
 }
---
+++ gamey-wsl/flk.nix
@@ -0,0 +1,30 @@
+{
+  self,
+  nixpkgs,
+  home-manager,
+  nixos-wsl,
+}:
+nixpkgs.lib.nixosSystem {
+  system = "x86_64-linux";
+  modules = [
+    ./configuration.nix
+    ../base.nix
+    ../flake-enablement.nix
+    home-manager.nixosModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.abentley = ../abentley.nix;
+      home-manager.users.root = ../root.nix;
+      home-manager.backupFileExtension = "backup";
+    }
+    nixos-wsl.nixosModules.default
+    {
+      system.stateVersion = "24.11";
+      wsl.enable = true;
+    }
+  ];
+  specialArgs = {
+    primaryUser = "abentley";
+  };
+}
