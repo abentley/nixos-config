55a4bdbbb9f8764f29ab68261e98c1ca6a05cc64
Aaron Bentley <aaron@aaronbentley.com>
2025-07-26 19:01:08 -0400

    I like big fonts and I cannot lie

--- skinny/configuration.nix
+++ skinny/configuration.nix
@@ -11,7 +11,16 @@
   ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    consoleMode = "0";
+  };
+  console = {
+    earlySetup = true; # Apply font early in boot process
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-i32b.psf.gz"; # Example: 32pt Terminus font
+    packages = with pkgs; [ terminus_font ]; # Ensure the font package is available
+    keyMap = "us"; # Your preferred keymap
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "skinny"; # Define your hostname.
--- skinny/hardware-configuration.nix
+++ skinny/hardware-configuration.nix
@@ -22,7 +22,7 @@
     "usb_storage"
     "sd_mod"
   ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "i915" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
