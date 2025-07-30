cfba79117c2716d00d5345c4127ac116ecb4ba89
Aaron Bentley <aaron@aaronbentley.com>
2025-07-30 18:22:04 -0400

    Add autorotate as a separate config.

--- abentley.nix
+++ abentley.nix
@@ -14,9 +14,14 @@
   '';
   home.file.".config/hypr/shared.conf".source = ./hyprland/shared.conf;
   home.file.".config/hypr/nix.conf".source = ./hyprland/nix.conf;
+  home.file.".config/hypr/autorotate.conf".source = ./hyprland/autorotate.conf;
   home.file.".config/waybar/config.jsonc".source = ./waybar/config.jsonc;
   home.file.".config/waybar/style.css".source = ./waybar/style.css;
   home.file.".config/waybar/power_menu.xml".source = ./waybar/power_menu.xml;
   home.file.".config/kitty/kitty.conf".source = ./kitty.conf;
   home.file.".local/share/applications/beeper.desktop".source = ./applications/beeper.desktop;
+  home.file.".config/walker/config.toml".source = ./walker/config.toml;
+  home.file.".config/walker/themes/default.toml".source = ./walker/themes/default.toml;
+  home.file.".config/walker/themes/default.css".source = ./walker/themes/default.css;
+  home.file.".config/walker/themes/default_window.toml".source = ./walker/themes/default_window.toml;
 }
