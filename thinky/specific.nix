35a632581453dfb9398daec24a73b05c915f2471
Aaron Bentley <aaron@aaronbentley.com>
2025-08-02 19:33:55 -0400

    Remove redundant stuff

--- thinky/specific.nix
+++ thinky/specific.nix
@@ -37,9 +37,6 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.desktopManager.gnome.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
