e644b6e6434119bb7cf8c57f5081745d80ecb74c
Aaron Bentley <aaron@aaronbentley.com>
2025-09-06 23:04:47 -0400

    Implement base configuration.

--- lappy/flk.nix
+++ lappy/flk.nix
@@ -10,16 +10,17 @@
 nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
   modules = [
+    ../base-configuration.nix
     ../suites/base.nix
     ../suites/graphical-computer.nix
     ../features/flake-enablement.nix
+    ./hardware-configuration.nix
     (
       { config, pkgs, ... }:
 
       {
         imports = [
           # Include the results of the hardware scan.
-          ./hardware-configuration.nix
         ];
 
         # Bootloader.
@@ -34,68 +35,8 @@ nixpkgs.lib.nixosSystem {
         # networking.proxy.default = "http://user:password@proxy:port/";
         # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-        # Enable networking
-        networking.networkmanager.enable = true;
-
-        # Set your time zone.
-        time.timeZone = "America/Toronto";
-
-        # Select internationalisation properties.
-        i18n.defaultLocale = "en_CA.UTF-8";
-
-        # Enable the X11 windowing system.
-        services.xserver.enable = true;
-
-        # Enable the GNOME Desktop Environment.
-        services.xserver.displayManager.gdm.enable = true;
-        services.xserver.desktopManager.gnome.enable = true;
-
-        # Configure keymap in X11
-        services.xserver.xkb = {
-          layout = "us";
-          variant = "";
-        };
-
-        # Enable CUPS to print documents.
-        services.printing.enable = true;
-
-        # Enable sound with pipewire.
-        hardware.pulseaudio.enable = false;
-        security.rtkit.enable = true;
-        services.pipewire = {
-          enable = true;
-          alsa.enable = true;
-          alsa.support32Bit = true;
-          pulse.enable = true;
-          # If you want to use JACK applications, uncomment this
-          #jack.enable = true;
-
-          # use the example session manager (no others are packaged yet so this is enabled by default,
-          # no need to redefine it in your config for now)
-          #media-session.enable = true;
-        };
-
         # Enable touchpad support (enabled default in most desktopManager).
         # services.xserver.libinput.enable = true;
-
-        # Define a user account. Don't forget to set a password with ‘passwd’.
-        users.users.abentley = {
-          isNormalUser = true;
-          description = "Aaron Bentley";
-          extraGroups = [
-            "networkmanager"
-            "wheel"
-          ];
-          packages = with pkgs; [
-            #  thunderbird
-          ];
-        };
-
-        # Install firefox.
-        programs.firefox.enable = true;
-
-        # Allow unfree packages
-        nixpkgs.config.allowUnfree = true;
 
         # List packages installed in system profile. To search, run:
         # $ nix search wget
