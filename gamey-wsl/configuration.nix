5477391be05745a51b0ae2d6a380099947b97ad5
Aaron Bentley <aaron@aaronbentley.com>
2025-09-13 12:34:54 -0400

    Include current config when building wsl tarballs

--- gamey-wsl/configuration.nix
+++ gamey-wsl/configuration.nix
@@ -19,6 +19,7 @@
 
   wsl.enable = true;
   wsl.defaultUser = "abentley";
+  wsl.tarball.configPath = ../.
   networking.hostName = "gamey-wsl";
 
   # This value determines the NixOS release from which the default
