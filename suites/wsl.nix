c07f0df6d9dcd546f9b8b4a9e259058cc5ad2e06
Aaron Bentley <aaron@aaronbentley.com>
2025-09-28 16:54:28 +0000

    Improve GUI app support

--- suites/wsl.nix
+++ suites/wsl.nix
@@ -14,29 +14,15 @@
   # Make life easier in the absence of gnome-keychain
   programs.ssh.extraConfig = "AddKeysToAgent yes";
   programs.bash.interactiveShellInit = ''eval "$(keychain --eval --quiet)"'';
-  wsl.enable = true;
-  wsl.defaultUser = primaryUser;
-  wsl.tarball.configPath = ../.;
-  systemd.tmpfiles.settings = {
-    # 1. Define a config file group name (e.g., "wsl-symlinks")
-    "wsl-symlinks" = {
-      # 2. Define the path (the key is the full path)
-      "/usr/share/applications" = {
-        # 3. Define the rule type (L+ for persistent symlink)
-        "L+" = {
-          # 4. Set the symlink target via the 'argument' attribute
-          argument = "/run/current-system/sw/share/applications";
-          # You can optionally define user, group, and mode here as well
-          # user = "root";
-          # group = "root";
-        };
-      };
 
-      "/usr/share/icons" = {
-        "L+" = {
-          argument = "/run/current-system/sw/share/icons";
-        };
-      };
+  wsl = {
+    enable = true;
+    defaultUser = primaryUser;
+    tarball.configPath = ../.;
+    nativeGuiApps = {
+      enable = true;
+      opengl.enable = true;
     };
+    startMenuLaunchers = true;
   };
 }
