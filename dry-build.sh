fa9b4b25c5d2f5629228de98bed149f3c0bb020b
Aaron Bentley <aaron@aaronbentley.com>
2025-10-11 15:33:13 -0400

    Actually build when running dry-build, just don't activate.

--- dry-build.sh
+++ dry-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eux
 for instance in hp handy gamey-wsl thinky thinky-wsl teeny skinny lappy; do
-    nixos-rebuild dry-build --flake .#${instance}
+    nixos-rebuild build --flake .#${instance}
 done
