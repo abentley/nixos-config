6ad9893f72deaaebe2301320f1a67c600da9afc8
Aaron Bentley <aaron@aaronbentley.com>
2026-05-10 12:22:24 -0400

    Add dmidecode support

--- physical-machine.nix
+++ physical-machine.nix
@@ -74,6 +74,7 @@ in
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
+    dmidecode
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
