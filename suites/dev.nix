9f13ffe7a1cc50cd741b43a8ae601d9c1ab3649a
Aaron Bentley <aaron@aaronbentley.com>
2026-07-12 23:34:20 -0400

    Add hexedit and xxd

--- suites/dev.nix
+++ suites/dev.nix
@@ -7,5 +7,7 @@ antigravity-nix:
     antigravity-nix.packages.x86_64-linux.default
     antigravity-nix.packages.x86_64-linux.google-antigravity-ide
     antigravity-nix.packages.x86_64-linux.google-antigravity-cli
+    hexedit
+    xxd
   ];
 }
